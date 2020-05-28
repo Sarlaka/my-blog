2ad718c44f89dc46da7bdcb55cb17052df0c9877
duchengdong <xinyiyake006@163.com>
2020-05-28 20:36:08 +0800

    push

---
+++ app.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: duchengdong
+ * @Date: 2020-05-28 20:35:43
+ * @LastEditors: duchengdong
+ * @LastEditTime: 2020-05-28 20:35:45
+ * @Description: 
+ */ 
+var spawn = require('child_process').spawn;
+
+free = spawn('hexo', ['server', '-p 4000']);/* 其实就是等于执行hexo server -p 4000*/
+
+free.stdout.on('data', function (data) {
+
+    console.log('standard output:\n' + data);
+
+});
+
+free.stderr.on('data', function (data) {
+
+    console.log('standard error output:\n' + data);
+
+});
+
+free.on('exit', function (code, signal) {
+
+    console.log('child process eixt ,exit:' + code);
+
+});
